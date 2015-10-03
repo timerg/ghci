b257b43e177f9508479d45242110c26c770a0b5a
timerg <ggteven@gmail.com>
2015-10-03 23:21:15 +0800

    ...

---
+++ excersize/T.hs
@@ -0,0 +1,51 @@
+data Cat = White Int | Black Int deriving (Show)
+
+paint :: Cat  -> Cat
+paint (White a) = Black a
+paint (Black a) = White a
+
+kick :: Cat  -> Cat
+kick (White a) = White $ a + 1
+kick (Black a) = Black $ a + 1
+
+data List a = Nil | Cons a (List a) deriving (Show)
+data List2 = Nil2 | Cons2 a deriving (Show)
+
+a0 :: List Int
+a0 = Nil
+
+a1 :: List Int
+a1 = Cons 0 a0
+
+a4 :: List Bool
+a4 = Cons False (Cons True (Cons True Nil))
+-- a2 :: List
+-- a2 = Cons 23 (Cons 18 Nil)
+--
+-- null' :: List -> Bool
+-- null' Nil = True
+-- null' _ = False
+
+length' :: List a -> Int
+length' Nil = 0
+length' (Cons _ a) = 1 + length' a
+--
+-- sum' :: List -> Int
+-- sum' Nil = 0
+-- sum' (Cons b a) = b  + sum' a
+
+data ListString = NilSt | ConsSt String ListString deriving (Show)
+
+
+
+
+
+
+
+
+
+
+
+
+
+--
--- excersize/Type.hs
+++ excersize/Type.hs
@@ -1,12 +1,23 @@
+module Shapes
+( Point(..)
+, Shape(..)
+, surface
+, baseCircle
+) where
+
+
+
 data Bool = False | True    -- part after "=" is  value constructors.
 data Shape = Circle Float Float Float | Rectangle Float Float Float Float
 -- ghci> :t Circle
-    Circle :: Float -> Float -> Float -> Shape
+
 -- So here value constructors is : Circle:: Float -> Float -> Float
   -- if we try to just print out Circle 10 20 5 in the prompt,
   --     because Haskell doesn't know how to display our data type as a string (yet).
-data Shape = Circle Float Float Float deriving(Show)
+data Shape2 = Circle2 Float Float Float deriving(Show)
 data Point = Point Float Float deriving(Show)
+
+data Shape3 Circle3 Point Float  deriving )
 
 baseCircle :: Float -> Shape
 baseCircle r = Circle (Point 0 0) r
@@ -14,11 +25,9 @@ baseCircle r = Circle (Point 0 0) r
 
 -- Export the functions and types that we defined here in a module: --
 
-module Shapes
-( Point(..)
-, Shape(..)
-, surface
-, nudge
-, baseCircle
-, baseRect
-) where
+
+data Maybe a = Nothing | Just a
+-- a here is the type parameter.
+-- Maybe a type constructor.
+
+data Cat
